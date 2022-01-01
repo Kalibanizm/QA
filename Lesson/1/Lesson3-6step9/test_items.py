66b89db7f6f57a418681112c4955d7a6307f638b
Kalibanizm <dpevb30@gmail.com>
2022-01-01 23:45:23 +0300

    Solve Pytest in 3.6 step9

---
+++ Lesson/1/Lesson3-6step9/test_items.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_basket_button(browser):
+    browser.get(link)
+    basket = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket') #Выбираем кнопку посредством селектора
+    assert basket #Проверяем присутствие элемента
